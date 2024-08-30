f50a61896943e7e9c51dd9b2666ee75a9bee5077
AustinRoverMini30 <114686049+AustinRoverMini30@users.noreply.github.com>
2024-08-30 11:51:47 +0200

    Correctif

--- src/main/java/com/example/ytdlp/TestAPI.java
+++ src/main/java/com/example/ytdlp/TestAPI.java
@@ -8,18 +8,16 @@ import java.util.List;
 
 public class TestAPI {
     public static void main(String[] args) throws JsonProcessingException {
-        YTDLP ytdlp = new YTDLP(".\\", "");
+        YTDLP ytdlp = new YTDLP(".\\", ".\\");
 
         ytdlp.update();
 
-        List<Media> test = ytdlp.createMedia("https://youtu.be/jO8gTE6tp3c?si=cSP2fcexZOGdouEQ");
+        List<Media> test = ytdlp.createMedia("https://youtu.be/rMIHDYCeaj4?si=unMXSNVDDO4HgJYA");
 
         for (Media media : test){
             media.setExt("mp3");
         }
 
         ytdlp.downloadMedia(test);
-
-        //System.out.println(ytdlp.getJson("https://youtu.be/jO8gTE6tp3c?si=cSP2fcexZOGdouEQ"));
     }
 }
